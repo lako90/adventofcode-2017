9934e59e325f3e0a241220d74bda419cd855471c
Luca Perna <luca.perna@thinkopen.it>
2017-12-05 10:55:22 +0100

    No express

--- index.js
+++ index.js
@@ -1,32 +1,20 @@
-const express = require('express');
 const inputs = require('./inputs.json');
+const input = inputs.day1;
+let result = 0;
+const toSum = [];
 
-const app = express();
-const port = 9090;
+for (let i = 0; i <= input.length; i += 1) {
+  const selfNumber = parseInt(input[i]);
+  let nextNumber = parseInt(input[i + 1]);
 
-app.get('/day1', function (req, res) {
-  const input = inputs.day1;
-  let result = 0;
-  const toSum = [];
-
-  for (let i = 0; i <= input.length; i += 1) {
-    const selfNumber = parseInt(input[i]);
-    let nextNumber = parseInt(input[i + 1]);
-
-    if (i === input.length - 1) {
-      nextNumber = parseInt(input[0]);
-    }
-
-    if (selfNumber === nextNumber) {
-      toSum.push(selfNumber);
-    }
+  if (i === input.length - 1) {
+    nextNumber = parseInt(input[0]);
   }
 
-  result = toSum.reduce((a, b) => a + b, 0);
+  if (selfNumber === nextNumber) {
+    toSum.push(selfNumber);
+  }
+}
 
-  res.send(String(result));
-});
-
-app.listen(port, function () {
-  console.log(`Example app listening on port ${port}!`);
-});
+result = toSum.reduce((a, b) => a + b, 0);
+console.log('RESULT', result);
